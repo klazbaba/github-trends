b0cb55a97a425f28738d455fc7c863f9a153c9ac
klaz <giwaklaz@gmail.com>
2018-08-08 22:18:41 +0100

    Initialized project with create-react-native-app.

---
+++ trendy/src/components/ListItem.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+import {color} from ../styles/color
+
+const ListItem = (props) => {
+    return (
+        <View style={styles.wrapper}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        borderBottomWidth: 1
+        borderBottomColor: color.grey
+    }
+})
